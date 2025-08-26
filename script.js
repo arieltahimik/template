159e319c58baec8b03bad4824a779c9d6d51d75d
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-08-26 17:28:05 -0500

    sample javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sample greet function
+
+function greet(name = "Guest") {
+  console.log(`Hello, ${name}!`);
+}
+
+greet();        // Output: Hello, Guest!
+greet("World"); // Output: Hello, World!
